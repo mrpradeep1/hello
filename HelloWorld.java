1d67089a710870e97597224a953d4552cac724a3
PradeepMohanRao <pradeep.mohanrao@gmail.com>
2023-11-04 00:28:04 +0530

    Added addition,sub & div

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,13 @@ class HelloWorld {
         int a = 5;
         int b = 10;
         int c = a*b;
+        int d = b-a;
+        int e = a+b;
+        int f = b/a
         System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
     }
 }
