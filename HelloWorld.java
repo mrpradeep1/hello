54932a6f064c3ad7de339c51fbe92efce2f3e7a7
PradeepMohanRao <pradeep.mohanrao@gmail.com>
2023-11-04 00:12:05 +0530

    added new feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        int a = 5;
+        int b = 10;
+        System.out.println(c);
     }
 }
